99b37abb7215e4d0466276a6a7d5aedcfb704c12
HassanKhan123 <hk2602809@gmail.com>
2020-12-07 21:59:09 +0500

    using prisma-bindings to run queries

--- src/prisma.js
+++ src/prisma.js
@@ -4,3 +4,13 @@ const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: 'http://localhost:4466/',
 });
+
+prisma.query.users(null, '{id name email posts {id title}}').then((data) => {
+  console.log(JSON.stringify(data, undefined, 2));
+});
+
+prisma.query
+  .comments(null, '{id text author {name} post {title}}')
+  .then((data) => {
+    console.log(JSON.stringify(data, undefined, 2));
+  });
