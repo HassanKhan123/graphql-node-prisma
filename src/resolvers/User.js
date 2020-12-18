84746c67d0080d8adfe3da38edff2f61d8da2f5e
HassanKhan123 <hk2602809@gmail.com>
2020-12-18 08:46:18 +0500

    locked down individual type fields

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,3 +1,12 @@
-const User = {};
+import getUserId from '../utils/getUserId';
+
+const User = {
+  email(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request, false);
+
+    if (userId && parent.id === userId) return parent.email;
+    return null;
+  },
+};
 
 export default User;
